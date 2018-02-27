6f59f66b4f3d64d060822106c33776cf7426a0fa
Manoj Talagadadeevi <manoj.talagadadeevi@ey.com>
2018-02-27 13:38:50 -0500

    Added `redux`, `redux-thunk`, `react-router-dom`

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, compose} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import {Router} from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={createBrowserHistory()}>
+            <App />
+        </Router>
+    </Provider>
+    , document.getElementById('root'));
+
 registerServiceWorker();
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
